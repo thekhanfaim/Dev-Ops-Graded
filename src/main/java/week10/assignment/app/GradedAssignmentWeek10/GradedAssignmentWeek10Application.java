17f494d8fb7eb42d72c49366922d870292ba64d0
Faim <faim.f@hcl.com>
2021-12-12 16:07:51 +0530

    Week 10 Assignment

---
+++ src/main/java/week10/assignment/app/GradedAssignmentWeek10/GradedAssignmentWeek10Application.java
@@ -0,0 +1,14 @@
+package week10.assignment.app.GradedAssignmentWeek10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedAssignmentWeek10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedAssignmentWeek10Application.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ src/test/java/week10/assignment/app/GradedAssignmentWeek10/GradedAssignmentWeek10ApplicationTests.java
@@ -0,0 +1,13 @@
+package week10.assignment.app.GradedAssignmentWeek10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradedAssignmentWeek10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
